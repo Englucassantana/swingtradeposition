90969f449f3061940f44b876c88f7d9885223616
Lucas <lucas3spro@gmail.com>
2021-11-18 20:25:02 -0300

    Adiçao de funçoes relativas ao carregamento dos pairs

--- public/JS/editLoadDataBase.js
+++ public/JS/editLoadDataBase.js
@@ -1,37 +1,164 @@
-let signal = {
-    "chartLink"  : "https://www.tradingview.com/x/76ZNUuZv/",
-    "firstToken" : "BAL",
-    "secondToken": "USDT",
-    "pair"       : "BALUSDT",
-    "buyZoneMin" : "24.741",
-    "buyZoneMax" : "26.470",
-    "reBuyMin"   : "20.307",
-    "reBuyMax"   : "22.010",
-    "targets"    : [
-    27.37,
-    29.121,
-    32.17,
-    38.762,
-    48.41,
-    68.378
-    ],
-    "stoploss"       : 24,
-    "riskLevel"      : "Moderado a alto",
-    "advice"         : "Saída parcial entre os alvos 1 e 2, 50%",
-    "isStPosition"   : true,
-    "exchange"       : "kucoin",
-    "tradingDuration": "1"
-}
+//*PROPERTIES
+let signal;
+let pairList = {
+  status: false,
+  data  : []
+};
 
-let pairList = {"status":true,"data":["1INCHUSDT","ADAUSDT","ALPHAUSDT","AXSUSDT","BANDUSDT","CHRUSDT","DYDXUSDT","ETHUSDT","FETUSDT","GRTUSDT","HBARUSDT","ICXUSDT","INJUSDT","ONEUSDT","RSRUSDT","SNXUSDT","UNFIUSDT","XTZUSDT","ZILUSDT","KSMUSDT","UNIUSDT","LINKUSDT","COMPUSDT","MANAUSDT"]}
+let maxShowedSignal   = 6;
+let lastShowedSignal  = -1;
+let firstShowedSignal = -1;
+
+const previousButtonId = 'previousButtonList';
+const nextButtonId     = 'nextButtonList';
+const pairListFrameId  = 'pair-selection';
+
+const previousButton = document.getElementById(previousButtonId);
+const nextButton     = document.getElementById(nextButtonId);
+const pairListFrame  = document.getElementById(pairListFrameId);
 
 let xhr = new XMLHttpRequest();
 
-xhr.onload = function (){
-  if(xhr.status === 200){
-    pairList = JSON.parse(xhr.responseText);
+//*FUNCTIONS
+function loadPairList(pairList, pairListFrame) {
+
+
+  if(pairList.length) {
+    pairList.forEach(pair => {            
+      const newSpan = document.createElement('span');
+
+      newSpan.innerText = pair;
+      pairListFrame.appendChild(newSpan);       
+    });
   }
 }
 
-xhr.open('GET', 'http://ec2-3-129-60-43.us-east-2.compute.amazonaws.com:10313/stposition/signals/pair?pair=grtusdt',true);
+function showPairList(pairListFrame) {
+  
+  let elSpan = pairListFrame.getElementsByTagName('span');
+
+  if(elSpan.length) {
+      firstShowedSignal = 0;
+
+      for (let index = 0; index < maxShowedSignal; index++) {
+        const span = elSpan[index];
+
+        span.style = 'display:inline';
+        lastShowedSignal++;
+      }
+
+      for (let index = 6; index < elSpan.length; index++) {
+        const span = elSpan[index];
+
+        span.style = 'display:none';
+      }
+  }  
+}
+
+function nextPairs() {
+  const pairListFrame = document.getElementById('pair-selection');
+
+  const elSpan  = pairListFrame.getElementsByTagName('span');
+  const remnant = elSpan.length - lastShowedSignal - 1;
+
+  if(remnant){        
+      for (let index = firstShowedSignal; index <= lastShowedSignal; index++) {
+        const span = elSpan[index];
+
+        span.style = 'display:none;';
+      }
+      firstShowedSignal = lastShowedSignal +1;
+
+      let newLastShowedSignal = lastShowedSignal;
+
+      if(remnant < maxShowedSignal){
+        for (let index = lastShowedSignal +1; index < lastShowedSignal +1 + remnant; index++) {
+          const span = elSpan[index];
+
+          span.style = 'display:inline;';
+          newLastShowedSignal++;        
+        }
+      }else{
+        for (let index = lastShowedSignal +1; index < lastShowedSignal +1 + maxShowedSignal; index++) {
+          const span = elSpan[index];
+
+          span.style = 'display:inline;';
+          newLastShowedSignal++;        
+        }
+      }
+      
+
+      lastShowedSignal = newLastShowedSignal;
+  } 
+}
+
+function previousPairs() {
+    const pairListFrame = document.getElementById('pair-selection');
+    const elSpan        = pairListFrame.getElementsByTagName('span');
+
+    if(firstShowedSignal){        
+        for (let index = firstShowedSignal; index <= lastShowedSignal; index++) {
+          const span       = elSpan[index];
+          span.style = 'display:none;';
+        }
+        lastShowedSignal     = firstShowedSignal - 1;
+        newFirstShowedSignal = firstShowedSignal;
+        for (let index = lastShowedSignal; index >= firstShowedSignal - maxShowedSignal; index--) {
+          const span       = elSpan[index];
+          span.style = 'display:inline;';
+          newFirstShowedSignal--;        
+        }
+        firstShowedSignal = newFirstShowedSignal;
+    }
+    console.log(`O primeiro sinal mostrado é o: ${firstShowedSignal}`);
+    console.log(`O ultimo sinal mostrado é o: ${lastShowedSignal}`);    
+}
+
+function getSignal(event) {
+  const elSpan = event.target;
+
+  if(elSpan.tagName.toUpperCase() == 'SPAN'){
+    let   xhr      = new XMLHttpRequest();
+    const url      = `http://ec2-3-129-60-43.us-east-2.compute.amazonaws.com:10313/stposition/signals/pair?pair=${pairName}`
+    const pairName = elSpan.innerText;
+
+    xhr.onload = function (){
+      if(xhr.status === 200){
+        signal = JSON.parse(xhr.responseText);   
+      }
+    }
+    xhr.open('GET', url , true);
+    xhr.send(null);
+    return true;
+  }
+  return false;
+}
+
+function loadTitlePair(){
+  const pairSelected = document.getElementById('pair-selected');
+
+  pairSelected           = pairSelected.getElementsByTagName('span')[0];
+  pairSelected.innerText = signal.pair
+}
+
+function showSignal(event) {
+  loadPairList();
+}
+
+//*EVENTS
+xhr.onload = function (){
+  if(xhr.status === 200){
+    pairList = JSON.parse(xhr.responseText);
+    loadPairList(pairList.data,pairListFrame)
+    showPairList(pairListFrame);      
+  }
+}
+nextButton.addEventListener('click', nextPairs);
+previousButton.addEventListener('click', previousPairs);
+pairListFrame.addEventListener('click', function (event) {
+  if(getSignal(event)) showSignal(event);
+});
+
+//*CODE
+xhr.open('GET', 'http://ec2-3-129-60-43.us-east-2.compute.amazonaws.com:10313/stposition/signals/',true);
 xhr.send(null);
